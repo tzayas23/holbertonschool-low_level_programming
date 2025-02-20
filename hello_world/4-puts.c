167636295e11b966ce81555c5631851eca6e9991
tzayas23 <tzayasbdgt@gmail.com>
2025-02-20 14:26:10 -0400

    Trying anopther C command

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*I am learning how to use C commands.
-*Return: Always 0
-*/
+ *I am learning how to use C commands.
+ *Return:
+ *Always 0
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
