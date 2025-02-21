21635f0d6e0b55fefbd8bab36e298d9a32f6ec96
tzayas23 <tzayasbdgt@gmail.com>
2025-02-21 10:11:13 -0400

    Trying a new command

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
-/** main-Learning more about C.
- *Return: Always 0 */
+/** 
+ * main - Learning more about C.
+ *Return: Always 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
