ba80419a907b5de6d53ffff58ec745ae2375d466
tzayas23 <tzayasbdgt@gmail.com>
2025-02-21 10:57:12 -0400

    Trying a new command c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /*
- * main - Learning how to do sizes on C.
+ * main
  * This program prints the size of various types
  * on the computer
  * Return: Always 0
  */
 
-int main(size)
+int main(void)
 {
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
