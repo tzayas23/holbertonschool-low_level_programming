0675c1f92b71b11af2a7660088c68d176d27c06d
tzayas23 <tzayasbdgt@gmail.com>
2025-02-24 11:49:17 -0400

    Trying a few commands

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -4,32 +4,30 @@
  * main - Entry point of the program
  *
  * Description: This program prints all possible combinations of
- * single-digit numbers, separated by ", ", in ascending order.
- * It uses only the putchar function four times and does not use
- * any variables of type char.
+ * single-digit numbers in ascending order, separated by ", ".
+ * It uses only the putchar function four times, and no `char` variables.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int num1, num2;
+    int num1, num2;
 
-for (num1 = 0; num1 < 9; num1++)
-{
-for (num2 = num1 + 1; num2 < 10; num2++)
-{
-putchar(num1 + '0');
-putchar(num2 + '0');
+    for (num1 = 0; num1 < 9; num1++)
+    {
+        for (num2 = num1 + 1; num2 < 10; num2++)
+        {
+            putchar(num1 + '0');
+            putchar(num2 + '0');
+            if (num1 != 8 || num2 != 9)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
 
-if (num1 != 8 || num2 != 9)
-{
-putchar(',');
-putchar(' ');
-}
-}
-}
+    putchar('\n');
 
-putchar('\n');
-
-return (0);
+    return (0);
 }
