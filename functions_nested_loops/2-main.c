ef6ee90e47efe347347b4c08ff210a6baea56901
tzayas23 <tzayasbdgt@gmail.com>
2025-02-24 17:44:28 -0400

    Trying another code

--- functions_nested_loops/2-main.c
+++ functions_nested_loops/2-main.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * main - Entry point to test print_alphabet_x10 function
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	print_alphabet_x10();
 	return (0);
+}
--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,19 @@
-#include <stdio.h>
+#include "main.h"
 
-void _putchar(char c) {
-	putchar(c);
+/**
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+    int i;
+    char letter;
+
+    for (i = 0; i < 10; i++)
+    {
+        for (letter = 'a'; letter <= 'z'; letter++)
+        {
+            _putchar(letter);
+        }
+        _putchar('\n');
+    }
 }
-
-void print_alphabet_x10(void) {
-	int i;
-	char letter;
-
-	for (i = 0; i < 10; i++) {
-		for (letter = 'a'; letter <= 'z'; letter++) {
-			_putchar(letter);
-		}
-		_putchar('\n');  // Print new line after each repetition
-	}
-}
-
-int main() {
-	print_alphabet_x10();  // Call the function to print the alphabet 10 times
-	return 0;
-}
-
