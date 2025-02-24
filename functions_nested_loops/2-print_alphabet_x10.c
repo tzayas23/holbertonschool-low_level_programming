f3253eb43dcdcbb950920a2da2978a364b9af005
tzayas23 <tzayasbdgt@gmail.com>
2025-02-24 17:45:17 -0400

    Trying another code

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -5,15 +5,15 @@
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    char letter;
+	int i;
+	char letter;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (letter = 'a'; letter <= 'z'; letter++)
-        {
-            _putchar(letter);
-        }
-        _putchar('\n');
-    }
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
 }
