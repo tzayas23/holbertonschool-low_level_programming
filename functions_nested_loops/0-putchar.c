6e414f3338f6d64422044643222e0cfad20308d3
tzayas23 <tzayasbdgt@gmail.com>
2025-02-24 12:21:24 -0400

    Trying a new program

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "_putchar", followed by a new line.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+printf("_putchar\n");
+
+return (0);
+}
